c22e357b95a0d0728c2fe3ea819ab828e688a864
Gylmsry.online <112378723+specialman12@users.noreply.github.com>
2024-03-15 15:41:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ $(document).ready(function() {
   });
   
   var typed = new Typed(".typing-3", {
-    strings: ["Developer "],
+    strings: ["Developer","❤️"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
